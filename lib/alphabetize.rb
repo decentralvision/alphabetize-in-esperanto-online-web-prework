eed4dc7c7f8fdb691801a64e1c785262d74b5648
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-11 02:35:00 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,11 +1,9 @@
 def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |string|
-
       string.each_char do |char|
         esperanto_alphabet.rindex(char)
       end
-
   end
 end
 
