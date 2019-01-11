fbfce331541a899779c0f44608f2d615ee07efb4
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-11 02:25:55 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,15 +1,6 @@
 def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by do |string|
-    words = string.split(' ')
-    words.each do |word|
-      score = 0
-      word.each_char do |letter|
-        esperanto_alphabet.rindex(letter)
-      end
-    end
-    score
-  end
+  arr.sort_by
 end
 
 example = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
